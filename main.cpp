142ab1ad1c4f94401fbf4c64708a51f202d2e1f3
David Lin <davidl16@illinois.edu>
2022-11-29 00:26:37 +0000

    parsed data.txt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
 #include "utils.h"
+#include <map>
+#include <algorithm>
+using namespace std;
 int main() {
-    std::string str = file_to_string("data.txt");
-    std::cout << str << std::endl;
+    string data = file_to_string("data.txt");
+    string routes = file_to_string("routes.txt");
+    vector<string> line;
+    int size = SplitString(routes, '\n', line);
+
+    map<string, vector<string>> route_maps;
+    for (int i = 0; i < size; i++) {
+        vector<string> vec;
+        SplitString(line.at(i), ',', vec);
+        vector<string> tmp = route_maps[vec.at(2)];
+        if (std::find(tmp.begin(), tmp.end(), vec.at(4)) == tmp.end()) {
+            route_maps[vec.at(2)].push_back(vec.at(4));
+        }
+    }
+    for (auto const& it : route_maps) {
+        cout << it.first << endl;
+        vector<string> tmp = it.second;
+        for (unsigned i = 0; i < tmp.size(); i++) {
+            cout << tmp.at(i) << " ";
+        }
+    }
 }
