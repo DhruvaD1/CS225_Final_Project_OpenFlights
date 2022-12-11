7f3f36933b1c710706b1fabe7a71993c2a7e1730
David Lin <davidl16@illinois.edu>
2022-12-11 10:59:04 +0000

    euler's path

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -1,7 +1,12 @@
 #include "bfs.h"
+#include <iostream>
+#include <cmath>
 
-vector<string> runBFS(map<string, vector<string>> route_maps, string starting_airport)
+vector<string> BFS::runBFS(map<string, vector<string>> route_maps, string starting_airport)
 {
+    out.clear();
+    in.clear();
+    path.clear();
     vector<string> airports_traversed;
     set<string> visited;
     
@@ -10,11 +15,15 @@ vector<string> runBFS(map<string, vector<string>> route_maps, string starting_ai
 
     while (!q.empty()) {
         string curr_airport = q.front();
+        //finds the number of airports that you can fly to from curr_airports
+        out[curr_airport] = route_maps[curr_airport].size();
         visited.insert(curr_airport);
         airports_traversed.push_back(curr_airport);
         q.pop();
 
         for (string neighbor : route_maps[curr_airport]) {
+            //find the number of airports that can fly to 
+            in[neighbor]+=1;
             if (visited.find(neighbor) == visited.end()) {
                 q.push(neighbor);
                 visited.insert(neighbor);
@@ -23,4 +32,73 @@ vector<string> runBFS(map<string, vector<string>> route_maps, string starting_ai
     } 
     
     return airports_traversed;
+}
+
+/**
+    @param routes - is a vector containing all the aiports in the dataset
+           start_airport - is the starting at which we determine whether or not a euler's path exists at that location
+    @return
+    path - a vector of strings containing the euler's path if there is one. If there isn't, return an empty vector
+*/
+vector<string> BFS::findEulerPath(map<string, vector<string>> routes, string start_airport) {
+    //runs a bfs to find the number of vertices going in and going out of each node
+    vector<string> bfs = runBFS(routes, start_airport);
+    if (hasEulerPath(bfs, start_airport) == 0) {
+        return vector<string>();
+    }
+    dfs(routes, start_airport);
+    return path;
+} 
+
+/** 
+    @param routes - map with all airports and their respective adjacency list
+           current_airport - the current airport we are at
+*/
+void BFS::dfs(map<string, vector<string>> routes, string current_airport) {
+    //adds the current_airport to the euler's path
+    path.push_back(current_airport);
+
+    while (out[current_airport] != 0) {
+        //finds the next airport to visit in the adjacency list and decrements out by 1
+        string next_airport = routes[current_airport].at(out[current_airport]-1);
+        out[current_airport]--;
+        //recursive call on the next airport
+        dfs(routes, next_airport);
+    }
+}
+/**
+    @param starting_airport - determine if a bfs exists at the starting airport
+           airports - vector of all airports
+    @return int - 0 if there is not an euler's path in the graph or if there is no euler's path starting
+    at starting_airport. 1 otherwise
+*/
+int BFS::hasEulerPath(vector<string> airports, string start_airport) {
+    //start node is any node with an extra out going edge, i.e. out = in+1
+    int start_nodes = 0;
+    //end node is any node with an extra incoming edge, i.e in = out+1
+    int end_nodes = 0;
+
+    for (unsigned i = 0; i < airports.size(); i++) {
+        string airport = airports.at(i);
+        /* if any node has more than one edge going in than it has edges going out or vice versa
+            than a euler's path does not exist */
+        if (std::abs(out[airport]-in[airport]) > 1) {
+            return false;
+        }
+        //iterates the number of start and end nodes in the graph
+        if (out[airport] == in[airport] + 1)
+            start_nodes++;
+        else if (out[airport] + 1 == in[airport])
+            end_nodes++;
+    }
+    /* An euler's circuit will exist if there are no start or end nodes detected. Therefore, the starting
+        airport will always have an euler's path. However, if there are start and end nodes, then 
+        the starting_airport will have one more edge going out than it will going in */
+    if ((start_nodes == 0 && end_nodes == 0)) {
+        return true;
+    } else if (out[start_airport] != in[start_airport] + 1) {
+        return false;
+    } 
+    //The last case of an euler's path existing is if there are exactly 1 start node and 1 end node
+    return (start_nodes == 1 && end_nodes == 1);
 }
--- tests/basic.cpp
+++ tests/basic.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 #include "bfs.h"
 
-TEST_CASE("Test1", "[valgrind][weight=1]"){
+TEST_CASE("Test1 BFS", "[valgrind][weight=1]"){
     SECTION("Basic BFS test cases work") {
         BFS test;
         vector<string> vec;
@@ -28,7 +28,7 @@ TEST_CASE("Test1", "[valgrind][weight=1]"){
         map["ORD"] = vec2;
 
 
-        vector<string> out = runBFS(map, "SFO");
+        vector<string> out = test.runBFS(map, "SFO");
 
         vector<string> ans;
         ans.push_back("SFO");
@@ -41,6 +41,8 @@ TEST_CASE("Test1", "[valgrind][weight=1]"){
 
         REQUIRE(out == ans);
     } SECTION("Harder BFS test cases work") {
+        BFS test;
+
         std::map<string, vector<string>> map;
 
         // SFO : {LAX, ORD}
@@ -60,7 +62,7 @@ TEST_CASE("Test1", "[valgrind][weight=1]"){
         vec3.push_back("SFO");
         map["LAX"] = vec3;
 
-        vector<string> out = runBFS(map, "SFO");
+        vector<string> out = test.runBFS(map, "SFO");
 
         vector<string> ans{"SFO", "LAX", "ORD", "JFK", "AER"};
         std::sort(out.begin(), out.end());
@@ -76,7 +78,7 @@ TEST_CASE("Test1", "[valgrind][weight=1]"){
         vec4.push_back("ORD");
         map["ADX"] = vec4;
         
-        vector<string> out2 = runBFS(map, "SFO");
+        vector<string> out2 = test.runBFS(map, "SFO");
         /* one way flights are not included in the bfs. for example, if SFO flies to ORD and 
         ADX flies to ORD then ADX will not be included in the BFS traversal stating at SFO */
         std::sort(out2.begin(), out2.end());
@@ -87,16 +89,15 @@ TEST_CASE("Test1", "[valgrind][weight=1]"){
         vec5.push_back("LAS");
         map["EBZ"] = vec5;
 
-        vector<string> out3 = runBFS(map, "SFO");
+        vector<string> out3 = test.runBFS(map, "SFO");
         std::sort(out3.begin(), out3.end());
 
         // Doesn't include aiports that have no key or values related to starting airport
         REQUIRE(out3 == ans);
     }
 }
-TEST_CASE("Test2", "[valgrind][weight=1]"){
+TEST_CASE("Test2 Data", "[valgrind][weight=1]"){
     SECTION("Parse data and creates adjancecy list") {
-        parseInput in;
 
         map<string, vector<string>> tmp = parseRoutes("../src/routes.txt");
 
@@ -116,5 +117,49 @@ TEST_CASE("Test2", "[valgrind][weight=1]"){
         std::sort(out2.begin(), out2.end());
 
         REQUIRE(ans2 == out2);
+    }
+}
+
+TEST_CASE("Test3 Eulerian Path", "[valgrind][weight=1]"){
+    SECTION("Test the Eulerian path algorithm") {
+        BFS b;
+        map<string, vector<string>> graph; 
+
+        vector<string> vec0;
+        vec0.push_back("1");
+        graph["0"] = vec0;
+
+        vector<string> vec1;
+        vec1.push_back("2");
+        graph["1"] = vec1;
+
+        vector<string> vec2;
+        vec2.push_back("0");
+        graph["2"] = vec2;
+
+        vector<string> vec3;
+        vec3.push_back("0");
+        vec3.push_back("4");
+        graph["3"] = vec3;
+
+        vector<string> vec4;
+        vec4.push_back("3");
+        graph["4"] = vec4;
+
+        /*running a bfs starting at 4 will create a graph with 
+            nodes 0,1,2,3,4 and an euler path will only exist starting at 3 */
+        vector<string> bfs = b.runBFS(graph, "4");
+        REQUIRE(b.hasEulerPath(bfs, "4") == 0);
+        REQUIRE(b.hasEulerPath(bfs, "3") == 1);
+        REQUIRE(b.hasEulerPath(bfs, "0") == 0);
+        //running a bfs starting at 0 will be a graph with nodes 0,1,2, and will be an euler circuit
+        bfs = b.runBFS(graph, "0");
+        REQUIRE(b.hasEulerPath(bfs, "0") == 1);
+        REQUIRE(b.hasEulerPath(bfs, "1") == 1);
+
+        vector<string> ans = b.findEulerPath(graph, "3");
+        for (unsigned i = 0; i <ans.size(); i++) {
+            cout << ans.at(i);
+        }
     }
 }
